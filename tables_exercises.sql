d8a208dc42a4b9c6c462e4a851c7a92bafce96a6
craigcalzado <craig.a.calzado@gmail.com>
2022-01-06 15:53:46 -0600

    Created tables_exercises.sql

---
+++ tables_exercises.sql
@@ -0,0 +1,18 @@
+SHOW DATABASES;
+
+USE employees;
+
+SHOW TABLES; 
+
+DESCRIBE employees; 
+
+DESCRIBE salaries;
+
+DESCRIBE titles; 
+DESCRIBE dept_emp;
+
+DESCRIBE dept_manager;
+
+DESCRIBE departments; DESCRIBE employees;
+
+SHOW CREATE TABLE dept_manager;
