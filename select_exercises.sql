197e8568b6cfa36039608529a7784fc072eaf77b
craigcalzado <craig.a.calzado@gmail.com>
2022-01-07 09:00:00 -0600

    Added the questions to the queries within select_exercises.sql

--- select_exercises.sql
+++ select_exercises.sql
@@ -2,40 +2,50 @@ SHOW DATABASES;
 
 USE albums_db;
 
+-- How many rows are in the albums table?
 SELECT MAX(id)
 FROM albums;
 
+-- How many unique artist names are in the albums table?
 SELECT COUNT(DISTINCT artist)
 FROM albums;
 
+-- What is the primary key for the albums table?
 SELECT id
 FROM albums;
 
+-- What is the oldest release date for any album in the albums table? What is the most recent release date?
 SELECT MIN(release_date)
 FROM albums;
 SELECT MAX(release_date)
 FROM albums;
 
+-- What is the oldest release date for any album in the albums table? What is the most recent release date?
 SELECT *
 FROM albums
 WHERE artist = 'Pink Floyd'; 
 
+-- The year Sgt. Pepper's Lonely Hearts Club Band was released?
 SELECT release_date
 FROM albums
 Where name = "Sgt. Pepper's Lonely Hearts Club Band";
 
+-- The genre for the album Nevermind?
 SELECT genre
 FROM albums
 Where name = "Nevermind";
 
+-- Which albums were released in the 1990s?
 SELECT *
 FROM albums
 WHERE release_date BETWEEN '1990' and '1999';
 
+-- Which albums had less than 20 million certified sales?
 SELECT * 
 FROM albums
 WHERE sales < '20.0';
 
+-- All the albums with a genre of "Rock".
 SELECT *
 FROM albums 
 WHERE genre = 'Rock';
