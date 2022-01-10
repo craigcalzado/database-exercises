d9cbc820a59fd45de561540624a418fe9559a068
craigcalzado <craig.a.calzado@gmail.com>
2022-01-10 08:52:44 -0600

    Finished functions_exercises.sql

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -31,4 +31,8 @@ SELECT MAX(salary), MIN(salary)
 FROM salaries;
 
 -- Use your knowledge of built in SQL functions to generate a username for all of the employees. A username should be all lowercase, and consist of the first character of the employees first name, the first 4 characters of the employees last name, an underscore, the month the employee was born, and the last two digits of the year that they were born. Below is an example of what the first 10 rows will look like:
-SELECT CONCAT(LOWER(first_name,1 ,1), LOWER(last_name, 1, 4), '_', (birth_date,6 ,2), (birth_date,2 ,2));
+
+SELECT LOWER(CONCAT(SUBSTR(first_name,1,1), SUBSTR(last_name,1,4),'_', SUBSTR(birth_date,6,2), SUBSTR(birth_date,3,2)))
+AS username, first_name, last_name, birth_date
+FROM employees
+LIMIT 10;
