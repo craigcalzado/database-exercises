b99bf4efe169026725e9fc751d1a805d516d40d9
craigcalzado <96085268+craigcalzado@users.noreply.github.com>
2022-06-16 10:20:08 -0500

    Going through SQL to sharpen my mind

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,14 +1,28 @@
+-- SHOW DATABASES; shows a list of databases avalible. 
 SHOW DATABASES;
 
+-- USE; selects the database you want to use.
 USE albums_db;
+USE home_credit;
 
 -- How many rows are in the albums table?
 SELECT MAX(id)
 FROM albums;
+-- How many rows are in the home_credit database?
+SELECT COUNT(*);
+-- This SQL query shows the number of tables within the database.
+-- Show all tables in home_credit 
+USE home_credit;
+SHOW TABLES;
+-- The SQL query above is useing home_credit database and howing all the tables within the database
 
 -- How many unique artist names are in the albums table?
 SELECT COUNT(DISTINCT artist)
 FROM albums;
+-- How man unique rows are there in application_history
+SELECT *
+FROM application_history;
+
 
 -- What is the primary key for the albums table?
 DESCRIBE albums;
