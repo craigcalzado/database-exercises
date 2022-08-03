e1147f1ae12bcfe15cef0f49113b29e4a28d3bef
craigcalzado <96085268+craigcalzado@users.noreply.github.com>
2022-08-03 11:59:08 -0500

    Updated the comments on the code to help understand what is going ou=n within the code.

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -5,20 +5,27 @@ SHOW DATABASES;
 -- Once you identified the database you utilize the USE command to call that data base.
 USE employees;
 
---Within the database you can view which tables are there to explore with the SHOW Tables function.
-show TABLES; 
+-- Within the database you can view which tables are there to explore with the SHOW Tables function.
+SHOW TABLES;
 
---
+-- Describe and then the database shows the name of the fields the type of data within that field if there are any nulls, shows any keys (primary or foreign).
 DESCRIBE employees; 
 
+-- Not ony can you use the describe function on the data base but on individual tables.
 DESCRIBE salaries;
 
-DESCRIBE titles; 
+-- Here we are going to look at multiple tables using the describe function.
+DESCRIBE titles;
+
 DESCRIBE dept_emp;
 
 DESCRIBE dept_manager;
 
-DESCRIBE departments; DESCRIBE employees;
+DESCRIBE departments;
+
+DESCRIBE employees;
+
 DESCRIBE dept_emp;
 
+-- The SHOW CREATE TABLE show the sql command that created the tables you select.
 SHOW CREATE TABLE dept_manager;
