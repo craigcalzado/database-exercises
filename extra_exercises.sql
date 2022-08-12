331ecfa56cbb47dd5993698d5027a63a93308786
craigcalzado <96085268+craigcalzado@users.noreply.github.com>
2022-08-12 12:56:49 -0500

    Initial commit for more exercises

---
+++ extra_exercises.sql
@@ -0,0 +1,7 @@
+-- Within this document we will be using the following terms: SELECT, DISTINCT, WHERE, IN, BETWEEN, LIKE, LIMIT, ORDER BY, and JOIN.
+
+-- 1. SELECT statements
+-- a. Select all columns from the actor table.
+USE sakila;
+SELECT * FROM actor;
+-- The query above returns all actors from the actor table within sakila database.
