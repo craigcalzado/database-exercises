f0746cd57f8915d4d7d6975eeeaa606d7556b0e5
craigcalzado <96085268+craigcalzado@users.noreply.github.com>
2022-08-12 13:01:22 -0500

    Added SELECT statements and SELECT DISTINCT operator

--- extra_exercises.sql
+++ extra_exercises.sql
@@ -3,5 +3,32 @@
 -- 1. SELECT statements
 -- a. Select all columns from the actor table.
 USE sakila;
-SELECT * FROM actor;
+SELECT * 
+FROM actor;
 -- The query above returns all actors from the actor table within sakila database.
+
+-- b. Select the first and last name columns from the actor table.
+SELECT first_name, last_name 
+FROM actor;
+-- The query above returns the first and last name columns from the actor table within sakila database.
+
+-- c. Select only the film_id, title, and release_year columns from the film table.
+SELECT film_id, title, release_year 
+FROM film;
+-- The query above returns the film_id, title, and release_year columns from the film table within sakila database.
+
+-- 2. DISTINCT operator
+-- a. Select all distinct (different) last names from the actor table.
+SELECT DISTINCT last_name
+FROM actor;
+-- The query above returns all distinct (different) last names from the actor table within sakila database.
+
+-- b. Select all distinct (different) postal codes from the address table.
+SELECT DISTINCT postal_code
+FROM address;
+-- The query above returns all distinct (different) postal codes from the address table within sakila database.
+
+-- c. Select all distinct (different) ratings from the film table.
+SELECT DISTINCT rating
+FROM film;
+-- The query above returns all distinct (different) ratings from the film table within sakila database.
